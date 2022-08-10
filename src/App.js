17a09af09b16b90f48db32ced38147ad3e3bb521
choichoijin <cuj10594@yonsei.ac.kr>
2022-08-10 15:23:08 +0900

    chapter 2

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+  display: flex;
+  background-color: ${(props) => props.theme.backgroundColor};
+`;
+
+const Title = styled.h1`
+  color: ${(props) => props.theme.textColor};
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Wrapper>
+      <Title>안녕하세요</Title>
+    </Wrapper>
   );
 }
 
