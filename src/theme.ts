85bb2b887393826bc946675e4db926c5a9024364
choichoijin <cuj10594@yonsei.ac.kr>
2022-08-12 01:31:51 +0900

    chapter 3

---
+++ src/theme.ts
@@ -0,0 +1,13 @@
+import { DefaultTheme } from "styled-components";
+
+export const ligntTheme: DefaultTheme = {
+  bgColor: "white",
+  textColor: "black",
+  btnColor: "tomato",
+};
+
+export const darkTheme: DefaultTheme = {
+  bgColor: "black",
+  textColor: "white",
+  btnColor: "teal",
+};
